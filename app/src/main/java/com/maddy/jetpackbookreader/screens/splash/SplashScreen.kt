63c070bceb1634b861330bcef6faa359729d12c0
M Isaq <maddy829019@gmail.com>
2024-01-17 13:25:04 +0530

    Created Splash Screen

--- app/src/main/java/com/maddy/jetpackbookreader/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/maddy/jetpackbookreader/screens/splash/SplashScreen.kt
@@ -1,13 +1,50 @@
 package com.maddy.jetpackbookreader.screens.splash
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.maddy.jetpackbookreader.R
 
 @Composable
 fun SplashScreen(navController: NavController) {
-    Column {
-        Text(text = "SplashScreen of Book Reader App")
+    Surface(
+        modifier = Modifier
+            .padding(15.dp)
+            .size(330.dp),
+        shape = CircleShape,
+        color = Color.White,
+        border = BorderStroke(width = 2.dp, color = Color.LightGray)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(R.string.logo),
+                style = MaterialTheme.typography.titleLarge,
+                color = Color.Red.copy(alpha = 0.5f)
+            )
+            Spacer(modifier = Modifier.height(15.dp))
+            Text(
+                text = stringResource(R.string.motto),
+                style = MaterialTheme.typography.headlineMedium,
+                color = Color.LightGray
+            )
+        }
     }
 }
