f41bddecc69df309aa7a4f85284a6402b909c1df
M Isaq <maddy829019@gmail.com>
2024-01-17 20:48:18 +0530

    Added Password TextField

--- app/src/main/java/com/maddy/jetpackbookreader/screens/login/LoginScreen.kt
+++ app/src/main/java/com/maddy/jetpackbookreader/screens/login/LoginScreen.kt
@@ -8,22 +8,28 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.maddy.jetpackbookreader.R
@@ -32,15 +38,21 @@ import com.maddy.jetpackbookreader.components.ReaderTopAppBar
 @Composable
 fun LoginScreen(navController: NavController) {
     var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    val passwordHidden = rememberSaveable { mutableStateOf(true) }
+    val visualTransformation =
+        if (passwordHidden.value) PasswordVisualTransformation() else VisualTransformation.None
+
+    //val passwordFocusRequest = FocusRequester.Default
 
     Scaffold(
         topBar = {
             ReaderTopAppBar(title = stringResource(R.string.logo_title)) { navController.popBackStack() }
         }
-    ) {
+    ) { paddingValues ->
         Surface(
             modifier = Modifier
-                .padding(it)
+                .padding(paddingValues)
                 .fillMaxSize()
         ) {
             Column(
@@ -55,7 +67,7 @@ fun LoginScreen(navController: NavController) {
                         .fillMaxWidth(),
                     enabled = true,
                     label = { Text(text = "Email") },
-                    placeholder = { Text(text = "google") },
+                    placeholder = { Text(text = "Enter email") },
                     keyboardOptions = KeyboardOptions(
                         keyboardType = KeyboardType.Email,
                         imeAction = ImeAction.Next
@@ -64,17 +76,38 @@ fun LoginScreen(navController: NavController) {
                     singleLine = true,
                     maxLines = 1,
                     shape = RoundedCornerShape(size = 15.dp),
-                    colors = OutlinedTextFieldDefaults.colors(
-                        focusedTextColor = Color.Blue,
-                        focusedContainerColor = Color.White,
-                        unfocusedContainerColor = Color.White,
-                        focusedBorderColor = Color.Blue,
-                        cursorColor = Color.Black,
-                        focusedPlaceholderColor = Color.LightGray,
-                        unfocusedPlaceholderColor = Color.Gray
+                )
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    modifier = Modifier
+                        .padding(horizontal = 10.dp)
+                        .fillMaxWidth(),
+                    enabled = true,
+                    label = { Text(text = "Password") },
+                    placeholder = { Text(text = "Enter password") },
+                    trailingIcon = { ShowTrailingIcon(passwordHidden) },
+                    visualTransformation = visualTransformation,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password,
+                        imeAction = ImeAction.Done
                     ),
+                    keyboardActions = KeyboardActions.Default,
+                    singleLine = true,
+                    maxLines = 1,
+                    shape = RoundedCornerShape(size = 15.dp),
                 )
             }
         }
+    }
+}
+
+@Composable
+private fun ShowTrailingIcon(passwordHidden: MutableState<Boolean>) {
+    IconButton(onClick = { passwordHidden.value = !passwordHidden.value }) {
+        val visibilityIcon = if (passwordHidden.value) Icons.Filled.Lock else Icons.Filled.Face
+        // Please provide localized description for accessibility services
+        val description = if (passwordHidden.value) "Show password" else "Hide password"
+        Icon(imageVector = visibilityIcon, contentDescription = description)
     }
 }
