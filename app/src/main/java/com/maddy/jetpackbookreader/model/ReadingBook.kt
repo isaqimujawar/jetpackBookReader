31a25374d9f1746a19fc9cf4bcbe112f512e820e
M Isaq <maddy829019@gmail.com>
2024-01-20 17:11:24 +0530

    Added Model Class for ReadingBook

---
+++ app/src/main/java/com/maddy/jetpackbookreader/model/ReadingBook.kt
@@ -0,0 +1,8 @@
+package com.maddy.jetpackbookreader.model
+
+data class ReadingBook(
+    var id: String? = null,
+    var title: String? = null,
+    var author: String? = null,
+    var notes: String? = null
+)
