9dc546e792f58d95b7cf1ee7ee31bec7d466272a
M Isaq <maddy829019@gmail.com>
2024-01-24 03:28:59 +0530

    Added Search Screen UI

--- app/src/main/java/com/maddy/jetpackbookreader/screens/search/SearchScreen.kt
+++ app/src/main/java/com/maddy/jetpackbookreader/screens/search/SearchScreen.kt
@@ -1,13 +1,29 @@
 package com.maddy.jetpackbookreader.screens.search
 
+import android.content.res.Configuration
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -15,15 +31,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.maddy.jetpackbookreader.R
+import com.maddy.jetpackbookreader.model.ReadingBook
+import com.maddy.jetpackbookreader.ui.theme.JetpackBookReaderTheme
+import com.maddy.jetpackbookreader.utils.getBook
 import com.maddy.jetpackbookreader.widgets.ReaderTopAppBar
 
 @OptIn(ExperimentalComposeUiApi::class)
@@ -66,7 +89,83 @@ fun SearchScreen(navController: NavController) {
                     maxLines = 1,
                     shape = RoundedCornerShape(size = 15.dp),
                 )
+                Spacer(Modifier.height(12.dp))
+                BookList(listOf(getBook(), getBook(), getBook(), ReadingBook()))
             }
         }
+    }
+}
+
+@Composable
+fun BookList(listOfBooks: List<ReadingBook>) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(horizontal = 8.dp, vertical = 16.dp)
+    ) {
+        items(items = listOfBooks) { book ->
+            NewBookCard(book) {
+                // TODO("Card onClick impl")
+            }
+        }
+    }
+}
+
+@Composable
+fun NewBookCard(book: ReadingBook, onClick: (String?) -> Unit = {}) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .clickable { onClick(book.id) },
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
+        elevation = CardDefaults.cardElevation(6.dp),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.books_unsplash),
+                modifier = Modifier
+                    .padding(end = 4.dp)
+                    .width(80.dp)
+                    .fillMaxHeight(),
+                contentDescription = stringResource(R.string.book_image),
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(text = book.title ?: "BookTitle")
+                Text(text = "Author: ${book.author ?: "Author"}")
+                Text(text = "Date: 2024-01-01")
+                Text(text = "[Computers]")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NewBookCardPreview() {
+    JetpackBookReaderTheme {
+        BookList(listOf(getBook(), getBook(), getBook(), ReadingBook()))
+    }
+}
+
+@Preview(name = "dark mode", uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun NewBookCardPreviewDark() {
+    JetpackBookReaderTheme {
+        BookList(listOf(getBook(), getBook(), getBook(), ReadingBook()))
     }
 }
