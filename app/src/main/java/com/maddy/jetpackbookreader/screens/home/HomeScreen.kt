3c617eff1c142fa26998be396da58de2e359b80e
M Isaq <maddy829019@gmail.com>
2024-01-23 14:05:01 +0530

    Added BookCard Composable

--- app/src/main/java/com/maddy/jetpackbookreader/screens/home/HomeScreen.kt
+++ app/src/main/java/com/maddy/jetpackbookreader/screens/home/HomeScreen.kt
@@ -1,14 +1,18 @@
 package com.maddy.jetpackbookreader.screens.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -16,6 +20,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.rounded.FavoriteBorder
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -30,23 +38,29 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.google.firebase.auth.FirebaseAuth
 import com.maddy.jetpackbookreader.R
+import com.maddy.jetpackbookreader.model.ReadingBook
 import com.maddy.jetpackbookreader.navigation.ReaderScreens
+import com.maddy.jetpackbookreader.ui.theme.JetpackBookReaderTheme
+import com.maddy.jetpackbookreader.utils.getBook
 
 @Composable
 fun HomeScreen(
-    navController: NavController,
-    viewModel: HomeViewModel = hiltViewModel()
+    navController: NavController, viewModel: HomeViewModel = hiltViewModel()
 ) {
     val displayName = viewModel.getUserDisplayName()
 
@@ -72,19 +86,14 @@ fun HomeScreen(
                 .padding(paddingValues = paddingValues)
                 .fillMaxSize()
         ) {
-            HomeContent(
-                navController,
-                displayName = displayName
-            )
+            HomeContent(navController, displayName = displayName)
         }
     }
 }
 
 @Composable
 fun HomeContent(
-    navController: NavController,
-    modifier: Modifier = Modifier,
-    displayName: String
+    navController: NavController, modifier: Modifier = Modifier, displayName: String
 ) {
     Column(
         modifier = modifier
@@ -105,8 +114,7 @@ fun HomeContent(
             Column(
                 modifier = Modifier.clickable {
                     navController.navigate(route = ReaderScreens.ReaderStatsScreen.name)
-                },
-                horizontalAlignment = Alignment.CenterHorizontally
+                }, horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Icon(
                     imageVector = Icons.Default.AccountCircle,
@@ -125,57 +133,168 @@ fun HomeContent(
                 Divider(modifier = Modifier.width(60.dp))
             }
         }
+        BooKCard(book = getBook())
+    }
+}
+
+@Composable
+fun BooKCard(book: ReadingBook, onClick: (String) -> Unit = {}) {
+    Card(
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth(0.9f)
+            .wrapContentHeight()
+            .clickable { onClick(book.id ?: "0") },
+        shape = RoundedCornerShape(29.dp),
+        elevation = CardDefaults.cardElevation(6.dp),
+    ) {
+        Column {
+            BookImageAndRating()
+            BookTitleAndAuthor()
+            ReadingSurface()
+        }
+    }
+}
+
+@Composable
+private fun BookImageAndRating() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.Top
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.books_unsplash),
+            contentDescription = stringResource(R.string.book_image),
+            modifier = Modifier
+                .fillMaxWidth(fraction = 0.8f)
+                .fillMaxHeight(),
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.FavoriteBorder,
+                contentDescription = stringResource(R.string.favorite_icon),
+                modifier = Modifier.padding(1.dp)
+            )
+            BookRating(4.5)
+        }
+    }
+}
+
+@Composable
+private fun ReadingSurface() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.End,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Surface(
+            Modifier.clip(RoundedCornerShape(topStartPercent = 29, bottomEndPercent = 29)),
+            color = MaterialTheme.colorScheme.primary
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Reading",
+                    modifier = Modifier.padding(8.dp),
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun BookTitleAndAuthor() {
+    Text(
+        text = "Book Title",
+        modifier = Modifier.padding(4.dp),
+        style = MaterialTheme.typography.headlineMedium,
+        fontWeight = FontWeight.SemiBold,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis
+    )
+    Text(
+        text = "Author",
+        modifier = Modifier.padding(start = 8.dp),
+        style = MaterialTheme.typography.labelLarge,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis
+    )
+}
+
+@Composable
+fun BookRating(rating: Double = 4.0) {
+    Surface(
+        shape = RoundedCornerShape(56.dp), shadowElevation = 6.dp
+    ) {
+        Column(
+            modifier = Modifier.padding(2.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.Star,
+                contentDescription = stringResource(R.string.star_icon)
+            )
+            Text(
+                text = rating.toString(), style = MaterialTheme.typography.headlineSmall
+            )
+        }
     }
 }
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 private fun HomeTopAppBar(navController: NavController, modifier: Modifier = Modifier) {
-    CenterAlignedTopAppBar(
-        title = {
-            Text(
-                text = "Book Reader",
-                style = MaterialTheme.typography.titleLarge,
-                color = MaterialTheme.colorScheme.primary,
-                fontWeight = FontWeight.SemiBold,
-            )
-        },
-        modifier = modifier
-            .padding(8.dp)
-            .shadow(elevation = 0.dp),
-        navigationIcon = {
-            Icon(
-                imageVector = Icons.Default.Menu,
-                contentDescription = stringResource(R.string.logo_icon)
-            )
-        },
-        actions = {
-            IconButton(
-                onClick = {
-                    FirebaseAuth.getInstance().signOut().run {
-                        navController.navigate(route = ReaderScreens.LoginScreen.name) {
-                            popUpTo(navController.graph.id) {
-                                inclusive = true
-                            }
-                        }
+    CenterAlignedTopAppBar(title = {
+        Text(
+            text = "Book Reader",
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.primary,
+            fontWeight = FontWeight.SemiBold,
+        )
+    }, modifier = modifier
+        .padding(8.dp)
+        .shadow(elevation = 0.dp), navigationIcon = {
+        Icon(
+            imageVector = Icons.Default.Menu,
+            contentDescription = stringResource(R.string.logo_icon)
+        )
+    }, actions = {
+        IconButton(onClick = {
+            FirebaseAuth.getInstance().signOut().run {
+                navController.navigate(route = ReaderScreens.LoginScreen.name) {
+                    popUpTo(navController.graph.id) {
+                        inclusive = true
                     }
                 }
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.logout_icon),
-                    contentDescription = stringResource(R.string.logout_icon)
-                )
             }
-        },
-        colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Transparent)
+        }) {
+            Icon(
+                painter = painterResource(id = R.drawable.logout_icon),
+                contentDescription = stringResource(R.string.logout_icon)
+            )
+        }
+    }, colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Transparent)
     )
 }
 
-/*
-@Preview(name = "home screen", showSystemUi = true)
+@Preview(showSystemUi = true)
 @Composable
-fun HomeScreenPreview() {
-    JetpackBookReaderTheme {
-       // HomeScreen()
-    }
-}*/
+fun BookCardPreview() {
+
+    JetpackBookReaderTheme { BooKCard(getBook()) }
+}
---
+++ app/src/main/java/com/maddy/jetpackbookreader/utils/ReaderUtils.kt
@@ -0,0 +1,10 @@
+package com.maddy.jetpackbookreader.utils
+
+import com.maddy.jetpackbookreader.model.ReadingBook
+
+fun getBook() = ReadingBook(
+    id = "1",
+    title = "Atomic Habits",
+    author = "James Clear",
+    notes = "Tiny Habits need to be established first before expanding."
+)
