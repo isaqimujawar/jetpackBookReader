2fa65b909fad697bc7524bb856f4889255d6d95d
M Isaq <maddy829019@gmail.com>
2024-01-27 19:08:31 +0530

    Added collectAsStateWithLifecycle()

--- app/src/main/java/com/maddy/jetpackbookreader/screens/details/BookDetailsScreen.kt
+++ app/src/main/java/com/maddy/jetpackbookreader/screens/details/BookDetailsScreen.kt
@@ -1,6 +1,5 @@
 package com.maddy.jetpackbookreader.screens.details
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,15 +9,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.maddy.jetpackbookreader.R
-import com.maddy.jetpackbookreader.model.Item
 import com.maddy.jetpackbookreader.widgets.ReaderTopAppBar
 
 @Composable
@@ -27,15 +25,6 @@ fun BookDetailsScreen(
     viewModel: BookDetailsViewModel = hiltViewModel(),
     bookId: String
 ) {
-    val bookItem = produceState<Item>(initialValue = Item()){
-        value = try {
-            viewModel.getBookInfo(bookId)
-        } catch (e: Exception) {
-            Log.d("BookDetailsScreen", "failed to get bookInfo()")
-            Item()
-        }
-    }.value
-
     Scaffold(
         topBar = {
             ReaderTopAppBar(title = stringResource(R.string.book_details)) {
@@ -55,15 +44,25 @@ fun BookDetailsScreen(
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                if (bookItem.volumeInfo?.title.isNullOrEmpty()) LinearProgressIndicator()
-                else ShowBookDetails(bookItem)
+                viewModel.getBookInfo(bookId).run { ShowBookDetails(viewModel) }
             }
         }
-
     }
 }
 
 @Composable
-private fun ShowBookDetails(bookItem: Item) {
-    Text(text = "${bookItem.volumeInfo?.title}")
+fun ShowBookDetails(viewModel: BookDetailsViewModel) {
+    val title = viewModel.title.collectAsStateWithLifecycle().value
+    val authors = viewModel.authors.collectAsStateWithLifecycle().value
+    val categories = viewModel.categories.collectAsStateWithLifecycle().value
+    val pageCount = viewModel.pageCount.collectAsStateWithLifecycle().value
+    val thumbnail = viewModel.thumbnail.collectAsStateWithLifecycle().value
+
+    if (title.isNullOrEmpty()) LinearProgressIndicator()
+
+    Text(text = title)
+    Text(text = authors)
+    Text(text = categories)
+    Text(text = pageCount)
+    Text(text = thumbnail)
 }
--- app/src/main/java/com/maddy/jetpackbookreader/screens/details/BookDetailsViewModel.kt
+++ app/src/main/java/com/maddy/jetpackbookreader/screens/details/BookDetailsViewModel.kt
@@ -1,14 +1,42 @@
 package com.maddy.jetpackbookreader.screens.details
 
+import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import com.maddy.jetpackbookreader.model.Item
+import androidx.lifecycle.viewModelScope
 import com.maddy.jetpackbookreader.repository.BookRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class BookDetailsViewModel @Inject constructor(
     private val repository: BookRepository,
+    private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
-    suspend fun getBookInfo(bookId: String): Item = repository.getBookInfo(bookId)
+
+      val title = savedStateHandle.getStateFlow(key = "title", initialValue = "")
+      val authors = savedStateHandle.getStateFlow(key = "authors", initialValue = "")
+      val categories = savedStateHandle.getStateFlow(key = "categories", initialValue = "")
+      val pageCount = savedStateHandle.getStateFlow(key = "pageCount", initialValue = "")
+      val thumbnail = savedStateHandle.getStateFlow(key = "thumbnail", initialValue = "")
+
+    init {
+        getBookInfo("")
+    }
+
+    fun getBookInfo(bookId: String) {
+        viewModelScope.launch {
+            try {
+                val item = repository.getBookInfo(bookId).volumeInfo
+                savedStateHandle["title"] = item?.title.toString()
+                savedStateHandle["authors"] = item?.authors.toString()
+                savedStateHandle["categories"] = item?.categories.toString()
+                savedStateHandle["thumbnail"] = item?.imageLinks?.thumbnail.toString()
+                savedStateHandle["pageCount"] = item?.pageCount.toString()
+            } catch (e: Exception) {
+                Log.d("BookDetailsViewModel", "getBookInfo:  exception = ${e.localizedMessage}")
+            }
+        }
+    }
 }
