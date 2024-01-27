3a64d45763b1edac168e24365dcf0d03f14242d5
M Isaq <maddy829019@gmail.com>
2024-01-27 16:50:50 +0530

    Using produceState to access uiState from BookDetailsViewModel

--- app/src/main/java/com/maddy/jetpackbookreader/screens/details/BookDetailsScreen.kt
+++ app/src/main/java/com/maddy/jetpackbookreader/screens/details/BookDetailsScreen.kt
@@ -1,5 +1,6 @@
 package com.maddy.jetpackbookreader.screens.details
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,6 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -25,8 +27,14 @@ fun BookDetailsScreen(
     viewModel: BookDetailsViewModel = hiltViewModel(),
     bookId: String
 ) {
-    viewModel.getBookInfo(bookId)
-    val bookItem = viewModel.bookItem
+    val bookItem = produceState<Item>(initialValue = Item()){
+        value = try {
+            viewModel.getBookInfo(bookId)
+        } catch (e: Exception) {
+            Log.d("BookDetailsScreen", "failed to get bookInfo()")
+            Item()
+        }
+    }.value
 
     Scaffold(
         topBar = {
@@ -47,7 +55,7 @@ fun BookDetailsScreen(
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                if (viewModel.isBookLoading) LinearProgressIndicator()
+                if (bookItem.volumeInfo?.title.isNullOrEmpty()) LinearProgressIndicator()
                 else ShowBookDetails(bookItem)
             }
         }
