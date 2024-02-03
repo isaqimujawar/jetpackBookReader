7ba51cbebe523281ae0ace957326cf4d19ad4741
M Isaq <maddy829019@gmail.com>
2024-02-03 20:38:02 +0530

    Added feature to delete book from firestore

--- app/src/main/java/com/maddy/jetpackbookreader/screens/update/UpdateScreen.kt
+++ app/src/main/java/com/maddy/jetpackbookreader/screens/update/UpdateScreen.kt
@@ -10,14 +10,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -100,9 +107,14 @@ fun ShowUpdateScreen(
         horizontalAlignment = Alignment.Start
     ) {
 
-        UpdateAndDeleteButton {
-            updateBook(navController, newHomeViewModel, context, book, updates)
-        }
+        UpdateAndDeleteButton(
+            onUpdateClick = {
+                updateBook(navController, newHomeViewModel, context, book, updates)
+            },
+            onDeleteClick = {
+                deleteBook(navController, newHomeViewModel, context, book)
+            }
+        )
         BookImageAndTitle(book) { navController.navigate(ReaderScreens.BookDetailsScreen.name + "/${book.googleBookId}") }
         BookRatingBar(text = "Your Rating", rating = yourRating) { newRating ->
             if (newRating != ratingState.intValue) {
@@ -123,7 +135,7 @@ fun ShowUpdateScreen(
 }
 
 @Composable
-fun UpdateAndDeleteButton(onUpdateClick: () -> Unit) {
+fun UpdateAndDeleteButton(onUpdateClick: () -> Unit, onDeleteClick:() -> Unit) {
     Row(
         modifier = Modifier
             .padding(12.dp)
@@ -131,11 +143,64 @@ fun UpdateAndDeleteButton(onUpdateClick: () -> Unit) {
         horizontalArrangement = Arrangement.SpaceEvenly,
         verticalAlignment = Alignment.CenterVertically
     ) {
+        // Update Button
         RoundedButton(text = "Update") {
             onUpdateClick()
         }
-        RoundedButton(text = "Delete")
+
+        // Delete Button
+        val openAlertDialog = rememberSaveable { mutableStateOf(false) }
+        when {
+            openAlertDialog.value -> {
+                ShowAlertDialog(
+                    dialogTitle = stringResource(id = R.string.are_you_sure),
+                    dialogText = stringResource(id = R.string.action_not_reversible),
+                    icon = Icons.Rounded.Info,
+                    onDismissRequest = { openAlertDialog.value = false },
+                    onConfirmation = {
+                        openAlertDialog.value = false
+                        onDeleteClick()         // Add logic here to handle confirmation.
+                    }
+                )
+            }
+        }
+        RoundedButton(text = "Delete") { openAlertDialog.value = true }
     }
+}
+
+@Composable
+fun ShowAlertDialog(
+    dialogTitle: String,
+    dialogText: String,
+    icon: ImageVector,
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit
+) {
+    AlertDialog(
+        icon = {
+            Icon(
+                imageVector = icon,
+                contentDescription = stringResource(R.string.alert_dialog_icon)
+            )
+        },
+        title = { Text(text = dialogTitle) },
+        text = { Text(text = dialogText) },
+        onDismissRequest = { onDismissRequest() },
+        confirmButton = {
+            TextButton(
+                onClick = { onConfirmation() }
+            ) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = { onDismissRequest() }
+            ) {
+                Text("Dismiss")
+            }
+        }
+    )
 }
 
 fun updateBook(
@@ -156,6 +221,26 @@ fun updateBook(
         } else {
             // Update failed
             Toast.makeText(context, "Book Update Failed", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+fun deleteBook(
+    navController: NavController,
+    newHomeViewModel: NewHomeViewModel,
+    context: Context,
+    book: ReadingBook
+) {
+    newHomeViewModel.deleteBook(book) { deleteSuccessful ->
+        if (deleteSuccessful) {
+            // Delete successful
+            Toast.makeText(context, "Book Deleted Successfully", Toast.LENGTH_SHORT).show()
+            navController.navigate(ReaderScreens.HomeScreen.name) {
+                popUpTo(navController.graph.id) { inclusive = true }
+            }
+        } else {
+            // Delete failed
+            Toast.makeText(context, "Book Delete Failed", Toast.LENGTH_SHORT).show()
         }
     }
 }
